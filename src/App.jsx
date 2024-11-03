e97ae93c4c8a911b4523250dff460ed5275ea8dd
Constantine Kobushka <kobushkaconstantine@gmail.com>
2024-11-03 14:12:49 +0200

    fixed App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,3 @@
-import { useState, useEffect } from 'react';
-
 import ContactForm from './components/ContactForm/ContactForm';
 import SearchBox from './components/SearchBox/SearchBox';
 import ContactList from './components/ContactList/ContactList';
@@ -7,19 +5,6 @@ import ContactList from './components/ContactList/ContactList';
 import styles from './App.module.css';
 
 function App() {
-  // function initialContacts() {
-  //   const contactsFromLocalStorage = localStorage.getItem('contactsData');
-  //   return contactsFromLocalStorage ? JSON.parse(contactsFromLocalStorage) : listcontacts;
-  // }
-
-  // const [searchValue, setSearchValue] = useState('');
-
-  // useEffect(() => {
-  //   localStorage.setItem('contactsData', JSON.stringify(contacts));
-  // }, [contacts]);
-
-  // const foundContacts = contacts.filter((contact) => contact.name.toLowerCase().includes(searchValue.toLowerCase()));
-
   return (
     <>
       <h1 className={styles.title}>Phonebook</h1>
