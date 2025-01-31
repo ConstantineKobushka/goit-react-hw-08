8a22472cc8df48ca9b640e9beeb254477653d9f9
Constantine Kobushka <kobushkaconstantine@gmail.com>
2025-01-31 14:20:06 +0200

    fixed NotFoundPage

--- src/pages/NotFoundPage/NotFoundPage.jsx
+++ src/pages/NotFoundPage/NotFoundPage.jsx
@@ -1,14 +1,15 @@
-import imgBg from '../../../public/img/not-found-page-bg.jpg';
-import imgBg2x from '../../../public/img/not-found-page-bg@2x.jpg';
-
 import css from './NotFoundPage.module.css';
 
 const NotFoundPage = () => {
   return (
     <div className={css.bg}>
       <picture>
-        <source media='(max-width: 1440px)' srcSet={`${imgBg} 1x, ${imgBg2x} 2x`} type='image/jpg' />
-        <img src={imgBg} alt='Page not found' loading='lazy' />
+        <source
+          media='(max-width: 1440px)'
+          srcSet='/img/not-found-page-bg.jpg 1x, /img/not-found-page-bg@2x.jpg 2x'
+          type='image/jpg'
+        />
+        <img src='/img/not-found-page-bg.jpg' alt='Page not found' />
       </picture>
     </div>
   );
